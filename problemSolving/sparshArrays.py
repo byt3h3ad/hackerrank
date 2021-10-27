bf8b5ec1496fca0ea9cac6904ba9888e8f0737cf
Adhiraj <adhirajd007@gmail.com>
2021-10-27 20:25:24 +0530

    Update sparshArrays.py

--- problemSolving/sparshArrays.py
+++ problemSolving/sparshArrays.py
@@ -1,8 +1,8 @@
 # https://www.hackerrank.com/challenges/sparse-arrays/problem
+
 def matchingStrings(strings, queries):
     matches = []
     for i in queries:
-        #print(i)
         count = 0
         for j in strings:
             if(i == j):
